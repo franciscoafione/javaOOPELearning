9416bc2762271129ec985c62fd9d44a8d486c3d5
francisco afione <francisco.afione@gmail.com>
2024-02-01 23:27:32 -0300

    Ready! the bug is fixed.

--- Main.java
+++ Main.java
@@ -2,38 +2,34 @@ package javaOOPELearning;
 
 import java.util.Scanner;
 
-
-
 public class Main {
 
-    public static Employee createEmployee(){
-        //It creates a new Employee.
+    public static Employee createEmployee() {
+        // It creates a new Employee.
+        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
         Employee employee;
         String name, location;
         int age;
         double salary;
-    
-        try{
-            System.out.println("Name:");
-            name = scanner.nextLine();
-            System.out.println("Age:");
-            age = scanner.nextInt();
-            System.out.println("Salary:");
-            salary = scanner.nextDouble();
-            System.out.println("Location:");
-            location = scanner.nextLine();
-        } finally{
-            scanner.close();
-        }
+
+        System.out.println("Name:");
+        name = scanner.nextLine();
+        System.out.println("Age:");
+        age = scanner.nextInt();
+        System.out.println("Salary:");
+        salary = scanner.nextDouble();
+        System.out.println("Location:");
+        location = scanner.nextLine();
 
         employee = new Employee(name, age, salary, location);
-        
+
         return employee;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
         Employee empA, empB;
         char option;
 
@@ -43,29 +39,25 @@ public class Main {
         System.out.println("Employee 2:");
         empB = createEmployee();
 
-
         System.out.println("Select wich employee is going to have a raise (a/b)");
-        try{
-            option = sc.next().charAt(0);
 
-            do{
-                if (option == 'a'){
-                    empA.raiseSalary(15.0);
-    
-                    System.out.println("Well, done!");
-                    System.out.println(empA + "'s salary was actualized to: $" + empA.getSalary());
-                } else if (option == 'b'){
-                    empB.raiseSalary(15.0);
-    
-                    System.out.println("Well, done!");
-                    System.out.println(empB + "'s salary was actualized to: $" + empB.getSalary());
-                } else {
-                    System.out.println("Invalid option. try again.");
-                }
-            } while (option != 'a' && option != 'b');
-        } finally{
-            sc.close();
-        }
+        option = sc.next().charAt(0);
+
+        do {
+            if (option == 'a') {
+                empA.raiseSalary(15.0);
+
+                System.out.println("Well, done!");
+                System.out.println(empA.getName() + "'s salary was actualized to: $" + empA.getSalary());
+            } else if (option == 'b') {
+                empB.raiseSalary(15.0);
+
+                System.out.println("Well, done!");
+                System.out.println(empB.getName() + "'s salary was actualized to: $" + empB.getSalary());
+            } else {
+                System.out.println("Invalid option. try again.");
+            }
+        } while (option != 'a' && option != 'b');
 
     }
 }
